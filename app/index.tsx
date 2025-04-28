81a45c5e6f79888e705b20a9d7cb572cef2c5857
Ahmed Abdelmageed <aabdelmageed@MacBook-Pro-Ahmed.local>
2025-04-29 00:45:07 +0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,16 @@
 import { View, Text } from "react-native";
 import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useNavigation, useRouter } from "expo-router";
+import { Redirect, useNavigation, useRouter } from "expo-router";
 
 const App = () => {
-  const router = useRouter();
-
   const token = false;
 
-  useEffect(() => {
-    if (token) {
-      router.push("/(tabs)");
-    } else {
-      router.push("/(auth)/login");
-    }
-  }, []);
+  if (!token) {
+    return <Redirect href={"/(auth)/login"} />;
+  }
 
-  return (
-    <SafeAreaView>
-      <Text>Index</Text>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/(tabs)"} />;
 };
 
 export default App;
